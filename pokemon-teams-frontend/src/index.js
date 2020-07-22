b5756cc0d396ca6c94902e46d534a58d7556a677
iona-b <ionabrabender@gmail.com>
2020-07-22 12:37:08 -0700

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -2,7 +2,7 @@ const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`   
 
-const fetchAllTrainers = () => {
+const fetchAllTrainers = () => {  
     fetch(`${TRAINERS_URL}`)
     .then(res => res.json())
     .then (json => json.forEach(trainer => showAllTrainers(trainer)))
